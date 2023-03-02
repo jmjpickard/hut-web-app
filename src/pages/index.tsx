a45f98e8453e3699755e8e8e73f171771ba37f30
Jack Pickard <jack@hertilityhealth.com>
2023-03-02 14:14:24 +0000

    lint fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       const newSelectedEvent = events?.find((event) => event.id === res.id);
       setSelectedEvent(newSelectedEvent);
     });
-  }, []);
+  }, [events]);
 
   useEffect(() => {
     if (upcomingState !== "selected") {
